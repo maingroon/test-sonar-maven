ed9a5ec03e0db99d5e97a0692df30d27a5ca82a2
maingroon <maingroon@gmail.com>
2022-08-14 21:15:24 +0300

    Test sonar

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,12 +2,13 @@ package org.example;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    public static int testTwo() {
+        return 2;
     }
 }
